0b3413a69836c038fad4819a86327c83ce4cbef6
Uni <cryptounico@gmail.com>
2021-04-30 05:23:26 -0500

    Update MakiswapFactory.sol

--- contracts/exchange/MakiswapFactory.sol
+++ contracts/exchange/MakiswapFactory.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity =0.5.16;
+pragma solidity >=0.6.6;
 
 import 'makiswap-core/contracts/interfaces/IMakiswapFactory.sol';
 import 'makiswap-core/contracts/MakiswapPair.sol';
 import 'makiswap-core/contracts/MakiswapHRC20.sol';
 
 contract MakiswapFactory is IMakiswapFactory {
-    address public feeTo;
-    address public feeToSetter;
+    address public override feeTo;
+    address public override feeToSetter;
     address public migrator;
 
-    mapping(address => mapping(address => address)) public getPair;
-    address[] public allPairs;
+    mapping(address => mapping(address => address)) public override getPair;
+    address[] public override allPairs;
 
-    event V2PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
     constructor(address _feeToSetter) public {
         feeToSetter = _feeToSetter;
     }
 
-    function allPairsLength() external view returns (uint) {
+    function allPairsLength() external override view returns (uint) {
         return allPairs.length;
     }
 
@@ -28,11 +28,11 @@ contract MakiswapFactory is IMakiswapFactory {
         return keccak256(type(MakiswapPair).creationCode);
     }
 
-    function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'MakiswapV2: IDENTICAL_ADDRESSES');
+    function createPair(address tokenA, address tokenB) external override returns (address pair) {
+        require(tokenA != tokenB, 'Makiswap: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'MakiswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'MakiswapV2: PAIR_EXISTS'); // single check is sufficient
+        require(token0 != address(0), 'Makiswap: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Makiswap: PAIR_EXISTS'); // single check is sufficient
         bytes memory bytecode = type(MakiswapPair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
@@ -45,18 +45,18 @@ contract MakiswapFactory is IMakiswapFactory {
         emit PairCreated(token0, token1, pair, allPairs.length);
     }
 
-    function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'MakiswapV2: FORBIDDEN');
+    function setFeeTo(address _feeTo) external override {
+        require(msg.sender == feeToSetter, 'Makiswap: FORBIDDEN');
         feeTo = _feeTo;
     }
 
     function setMigrator(address _migrator) external {
-        require(msg.sender == feeToSetter, 'MakiswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'Makiswap: FORBIDDEN');
         migrator = _migrator;
     }
 
-    function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'MakiswapV2: FORBIDDEN');
+    function setFeeToSetter(address _feeToSetter) external override {
+        require(msg.sender == feeToSetter, 'Makiswap: FORBIDDEN');
         feeToSetter = _feeToSetter;
     }
 
